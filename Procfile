45ce16fc0276c5a2cb1daebdb5c0288ef2a7900d
HOLYKEYZ <ayandajoseph390@gmail.com>
2025-12-09 14:55:50 +0100

    render-deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn digitalshield.wsgi:application --bind 0.0.0.0:5000
+web: gunicorn digitalshield.wsgi:application
--- digitalshield/urls.py
+++ digitalshield/urls.py
@@ -3,8 +3,13 @@ URL configuration for digitalshield project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
+    path('health/', health_check),
     path('admin/', admin.site.urls),
     path('', include('scanner.urls')),
     path('accounts/', include('accounts.urls')),
@@ -13,4 +18,3 @@ urlpatterns = [
     # REST API v1
     path('api/v1/', include('scanner.api.urls')),
 ]
-
